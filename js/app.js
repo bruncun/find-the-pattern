f2280ee407f5418ae3c036fc2785dbdbd8f68324
bruncun <bruncun@icloud.com>
2017-09-17 11:16:31 -0400

    Add random tile

--- js/app.js
+++ js/app.js
@@ -1,56 +1,92 @@
 (function() {
   "use strict";
 
-  angular.module("findThePattern", []);
-
-  angular.module("findThePattern").controller("MainController", MainController);
-
-  MainController.$inject = [];
+  angular
+    .module("findThePattern", [])
+    .controller("MainController", MainController)
+    .directive("randomTile", randomTile)
+    .constant("icons", [
+      "glyphicon-leaf",
+      "glyphicon-fire",
+      "glyphicon-flash",
+      "glyphicon-tent",
+      "glyphicon-remove",
+      "glyphicon-plus"
+    ])
+    .constant("colors", [
+      "text-danger",
+      "text-success",
+      "text-info",
+      "text-muted",
+      "text-warning",
+      "text-primary"
+    ]);
 
   function MainController() {
     var vm = this;
-    vm.lives = 3;
+    vm.lives = 5;
+    vm.round = 1;
     vm.tiles = [
       {
-        icon: "leaf",
-        color: "text-success"
-      },
-      null,
-      {
-        icon: "fire",
-        color: "text-warning"
-      },
-      null,
-      null,
-      {
-        icon: "ice-lolly-tasted",
-        color: "text-danger"
-      },
-      null,
-      {
-        icon: "flash",
-        color: "text-warning"
+        type: "pattern"
       },
       {
-        icon: "hourglass",
-        color: "text-primary"
-      },
-      null,
-      null,
-      {
-        icon: "eye-open",
-        color: "text-muted"
-      },
-      null,
-      {
-        icon: "flash",
-        color: "text-warning"
-      },
-      {
-        icon: "ice-lolly-tasted",
-        color: "text-danger"
+        type: "random"
       },
       null
     ];
+  }
+
+  randomTile.$inject = ["$interval", "icons", "colors"];
+
+  function randomTile($interval, icons, colors) {
+    function link(scope, element, attrs) {
+      var face = {
+        icon: "",
+        color: ""
+      };
+
+      function activate() {
+        $interval(update, 1000);
+
+        update();
+      }
+
+      function getRandomFace() {
+        return {
+          icon: icons[Math.floor(Math.random() * icons.length)],
+          color: colors[Math.floor(Math.random() * icons.length)]
+        };
+      }
+
+      function isFaceDifferent(newFace) {
+        return newFace.icon === face.icon || newFace.color === face.color;
+      }
+
+      function replace(newFace) {
+        element.removeClass(face.icon + " " + face.color);
+        element.addClass(newFace.icon + " " + newFace.color);
+
+        face.icon = newFace.icon;
+        face.color = newFace.color;
+      }
+
+      function update() {
+        var newFace = getRandomFace();
+
+        if (isFaceDifferent(newFace)) {
+          update();
+          return;
+        }
+
+        replace(newFace);
+      }
+
+      activate();
+    }
+
+    return {
+      link: link
+    };
   }
 })();
